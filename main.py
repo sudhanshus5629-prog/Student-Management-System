91931104559c385053d3d496198def559b2ba98a
Sudhanshu Sharma <sudhanshus5629@gmail.com>
2026-07-30 20:29:11 +0530

    Add duplicate roll number validation

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 
 class Student:
 
-    def __init__(self, name,roll_number, marks,grade, fees):
+    def __init__(self, name,roll_number, marks,grade, fees, delete):
         self.name = name
         self.roll_number = roll_number
         self.marks = marks
         self.grade = grade
         self.fees = fees
+        self.delet = delete
         
 students = []
 
 
-def add_student():  
- 
 
+
+def add_student():  
+   
+      
     name = input("Enter the name:")
     try:
       roll_number = int(input("Enter the roll_number:"))
+    except ValueError:
+        print("Invalid number!")
+        return
+    for s in students:
+        if s.roll_number == roll_number:
+            print("This roll number are already avalable")
+            return
+        
+    try:
       marks = int(input("Enter the marks:"))
       fees = float(input("Enter the fees:"))
     except ValueError:  
         print("\ninvalid input for roll number marks, or fees Please enter numbers.")
         return
     grade = input("Enter thr grade:")
-    s = Student(name, roll_number, marks, grade, fees)
+    delete_student = int(input("Enter the delete student rill number :"))
+    s = Student(name, roll_number, marks, grade, fees, delete_student)
     students.append(s)
     print("\nStudent Added Successfully!")
 
@@ -69,30 +82,56 @@ def update_student():
     try:
         roll = int(input("Enter the update roll number:")) 
     except ValueError:
-        print("Envalid roll number")
+        print("Invalid roll number. Please enter a number.")
         return 
+
     for s in students:
         if s.roll_number == roll:
-            s.name = input("Enter the new name:")
-            s.marks = input("Enter the new marks:")
-            s.grade = input("Enter the new grade:")
-            s.fees = input("Enter the new fees:")
-
-            print("Update successfully!")
+            try:
+                s.name = input("Enter the new name: ")
+                s.marks = int(input("Enter the new marks: "))
+                s.grade = input("Enter the new grade: ")
+                s.fees = float(input("Enter the new fees: "))
+                print("\nStudent details updated successfully!")
+            except ValueError:
+                print("\nInvalid input for marks or fees. Please enter numbers.")
             return 
-        print("student not found.")
-                
 
+    print(f"\nStudent with roll number {roll} not found.")
+
+def delete_student():
+    if not students:
+        print("\nNo student to delete.")
+        return
+    
+    try:
+        roll_to_delete = int(input("Enter the roll number student for delete :"))
+    except ValueError:
+        print("Invaild input. please enter a roll number")
+        return 
+    found = False
+    for s in students:
+        if s.roll_number == roll_to_delete:
+            students.remove(s)
+            print(f"\nstudent with roll number{roll_to_delete} delete successfilly!")
+            Fount = True
+            break
+        not found
+        print(f"\nNo students fiond with roll number{roll_to_delete}")
+    
 
 while True:
-    print("\n--- Student Management System ---")
+    print("\n----Student Management System----\n")
+    
     print("1. Add Student")
     print("2. Display Students")
     print("3. Search Student")
     print("4. Update Student")
-    print("5. Exit")
-    choice = input("Enter your choice (1-5): ")
+    print("5. delete student")
+    print("6. Exit")
+    choice = input("Enter your choice (1-6): ")
 
+    
     if choice == '1':
         add_student()
     elif choice == '2':
@@ -101,11 +140,13 @@ while True:
         search_student()
     elif choice == '4':
         update_student()
-
-    elif choice == '5':      
+    elif choice == '5':  
+        delete_student()
+    elif choice == '6':
         print("Exiting Program. Goodbye!")
         break
     else: 
-        print("Invalid choice. Please enter a number between 1 and 4.")       
+        print("Invalid choice. Please enter a number between 1-6.")  
+          
          
   
